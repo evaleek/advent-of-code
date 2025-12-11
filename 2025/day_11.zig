e5dba48cfbff1cce7bc2beb1a4bbc3717e9a32ae
evaleek <dev@evii.li>
2025-12-11 16:07:06 -0500

    Add unfinished 2025 Day 11 Part 2

--- 2025/day_11.zig
+++ 2025/day_11.zig
@@ -51,6 +51,108 @@ fn pathsToLabel(map: PathMap, from: Label, to: Label) u16 {
     }
 }
 
+test part2 {
+    const example =
+        \\svr: aaa bbb
+        \\aaa: fft
+        \\fft: ccc
+        \\bbb: tty
+        \\tty: ccc
+        \\ccc: ddd eee
+        \\ddd: hub
+        \\hub: fff
+        \\eee: dac
+        \\dac: fff
+        \\fff: ggg hhh
+        \\ggg: out
+        \\hhh: out
+        \\
+    ;
+    const answer = 2;
+    try testing.expectEqual(answer, try part2(example));
+}
+
+pub fn part2(input: []const u8) !u16 {
+    map_buffer = undefined;
+    const start_key: Label = .{ 's', 'v', 'r' };
+    const end_key: Label = .{ 'o', 'u', 't' };
+
+    var fba: std.heap.FixedBufferAllocator = .init(&map_buffer);
+    const allocator = fba.allocator();
+
+    const path_map = try parse(input, allocator);
+    var path_traversal: PathTraversal = .empty;
+    var resolved_paths: PathTraversalMemoization = .empty;
+
+    const contains_result = try pathsToLabelContaining(
+        allocator,
+        path_map,
+        &path_traversal,
+        &resolved_paths,
+        start_key,
+        end_key,
+        .{ .{ 'd', 'a', 'c' }, .{ 'f', 'f', 't' } },
+    );
+
+    assert(path_traversal.count() == 0);
+
+    return contains_result.both;
+}
+
+/// Currently does not work.
+fn pathsToLabelContaining(
+    allocator: mem.Allocator,
+    map: PathMap,
+    current_path: *PathTraversal,
+    path_dp: *PathTraversalMemoization,
+    from: Label,
+    to: Label,
+    contains: [2]Label,
+) mem.Allocator.Error!ContainsResult {
+    //for (current_path.keys()) |key| std.debug.print("{s}->", .{key});
+    //std.debug.print("{s}\n", .{from});
+
+    const contains_1 = current_path.contains(contains[0]);
+    const contains_2 = current_path.contains(contains[1]);
+
+    if (labelEql(from, to)) {
+        return .{
+            .none = @intFromBool(   !contains_1 and !contains_2 ),
+            .first = @intFromBool(   contains_1 and !contains_2 ),
+            .second = @intFromBool( !contains_1 and  contains_2 ),
+            .both = @intFromBool(    contains_1 and  contains_2 ),
+        };
+    } else {
+        if (!current_path.contains(from)) {
+            const outputs: []const Label = map.get(from) orelse return .empty;
+
+            try current_path.putNoClobber(allocator, from, {});
+
+            const total = if (path_dp.get(from)) |dp| dp else recurse: {
+                var t: ContainsResult = .empty;
+                for (outputs) |output| t = t.add(try pathsToLabelContaining(
+                    allocator,
+                    map,
+                    current_path,
+                    path_dp,
+                    output,
+                    to,
+                    contains,
+                ));
+                try path_dp.putNoClobber(allocator, from, t);
+                break :recurse t;
+            };
+
+            const pop = current_path.pop();
+            assert(pop != null and labelEql(pop.?.key, from));
+
+            return total.prefixed(contains_1, contains_2);
+        } else {
+            return .empty;
+        }
+    }
+}
+
 fn parse(input: []const u8, allocator: mem.Allocator) !PathMap {
     var path_map: PathMap = .empty;
 
@@ -96,10 +198,72 @@ fn labelEql(lhs: Label, rhs: Label) bool {
 }
 
 // Hash maps are big, it turns out
-var map_buffer: [65536]u8 = undefined;
+var map_buffer: [131072]u8 = undefined;
 
 const Label = [3]u8;
 const PathMap = std.hash_map.AutoHashMapUnmanaged(Label, []const Label);
+const PathTraversal = std.array_hash_map.AutoArrayHashMapUnmanaged(Label, void);
+const PathTraversalMemoization = std.hash_map.AutoHashMapUnmanaged(Label, ContainsResult);
+const ContainsResult = struct {
+    none: u16,
+    first: u16,
+    second: u16,
+    both: u16,
+
+    const empty: ContainsResult = .{
+        .none = 0,
+        .first = 0,
+        .second = 0,
+        .both = 0,
+    };
+
+    fn prefixed(contains: ContainsResult, has_first: bool, has_second: bool) ContainsResult {
+        if (has_first and has_second) {
+            return .{
+                .none = 0,
+                .first = 0,
+                .second = 0,
+                .both = contains.none + contains.first + contains.second + contains.both,
+            };
+        } else if (has_first) { // but not second
+            return .{
+                .none = 0,
+                .first = contains.first + contains.none,
+                .second = 0,
+                .both = contains.both + contains.second,
+            };
+        } else if (has_second) { // but not first
+            return .{
+                .none = 0,
+                .first = 0,
+                .second = contains.second + contains.none,
+                .both = contains.both + contains.first,
+            };
+        } else {
+            return contains;
+        }
+
+        //return .{
+        //    .none = contains.none * @intFromBool(!(has_first or has_second)),
+        //    .first = contains.first * @intFromBool(!has_second)
+        //        + contains.none * @intFromBool(has_first and !has_second),
+        //    .second = contains.second * @intFromBool(!has_first)
+        //        + contains.none * @intFromBool(has_second and !has_first),
+        //    .both = contains.both
+        //        + @intFromBool(has_second) * contains.first
+        //        + @intFromBool(has_first) * contains.second,
+        //};
+    }
+
+    fn add(lhs: ContainsResult, rhs: ContainsResult) ContainsResult {
+        return .{
+            .none = lhs.none + rhs.none,
+            .first = lhs.first + rhs.first,
+            .second = lhs.second + rhs.second,
+            .both = lhs.both + rhs.both,
+        };
+    }
+};
 
 const assert = std.debug.assert;
 const testing = std.testing;
